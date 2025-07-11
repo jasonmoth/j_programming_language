2e0fd18a0509276283bf66add6446127725afea6
Jason McAdam <jasonmcadam16@gmail.com>
2025-07-10 22:37:46 -0400

    Update

--- j_compiler.c
+++ j_compiler.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 typedef struct Node {
 	char *value;
@@ -22,15 +23,15 @@ void push_to_stack(Stack *s, const char *str) {
 
 	Node *new_node = malloc(sizeof(Node));
 	if (!new_node) {
-		fprintf(stderr, "Error: malloc failed!\n");
-	exit(1);
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(1);
 	}
 
 	new_node->belowNodeAddress = s->topNodeAddress;
 
 	new_node->value = malloc(strlen(str) + 1);
 	if (!new_node->value) {
-		fprintf(stderr, "Error: malloc failed!\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(1);
 	}
 
@@ -40,15 +41,15 @@ void push_to_stack(Stack *s, const char *str) {
 
 char *pop_from_stack(Stack *s) {
 
-	if (s->topNodeAddress == NULL) {
-		return NULL;
-	}
+    if (is_stack_empty(s)) {
+        fprintf(stderr, "Error: Attempted to pop from empty stack\n");
+        exit(1);
+    }
 
 	Node *old_top_node_address = s->topNodeAddress;
 	char *old_top_node_value_address = old_top_node_address->value;
 	s->topNodeAddress = old_top_node_address->belowNodeAddress;
 	free(old_top_node_address);
-	printf("Pop!\n");
 	return old_top_node_value_address;
 }
 
@@ -151,11 +152,23 @@ char *convert_char_to_string(char ch) {
     return str;
 }
 
+bool is_numeric_string(const char *str) {
+    if (str == NULL || *str == '\0') return false;  // empty string
+
+    if (*str == '-') str++;
+
+    while (*str) {
+        if (!isdigit(*str)) return false;
+        str++;
+    }
+    return true;
+}
+
 
 int main(int argc, char *argv[]) {
 
 
-	// perform_stack_unit_tests();
+	perform_stack_unit_tests();
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <filename>\n", argv[0]); // argv[0] is always reserved for the name of THIS FILE
@@ -169,17 +182,46 @@ int main(int argc, char *argv[]) {
 
 	Stack *s = initialize_stack();
 
-	char ch;
-
-	while((ch = fgetc(file)) != EOF) { 
+	char file_char_read;
+	while((file_char_read = fgetc(file)) != EOF) { 
 		printf("%c", ch);
-		if (ch == ' ') continue; // TODO revisit this when you realize you are tokenizing a string that may contain actual spaces
-		push_to_stack(s, convert_char_to_string(ch));
+
+		if (file_char_read == ' ') {
+			continue;
+		} else {
+			push_to_stack(s, convert_char_to_string(file_char_read)); // revist when interpreting string literals
+		}
+		
 	}
-
-	print_and_pop_entire_stack(s);
-
+	
 	fclose(file);
+
+	char *stack_read;
+	while(!is_stack_empty(s)) {
+		stack_read = pop_from_stack(s);
+		if (strcmp(stack_read, "+") == 0) {
+			char *right_summand = pop_from_stack(s);
+			char *left_summand  = pop_from_stack(s);
+
+			if (!is_numeric_string(right_summand) || !is_numeric_string(left_summand)) {
+				fprintf(stderr,
+					"Error: Invalid sum expression.\n"
+					"  Left:  %s\n"
+					"  Right: %s\n"
+					"  Operator: %s\n",
+					left_summand, right_summand, stack_read);
+
+				exit(1);
+			}
+
+			printf("%d\n", atoi(left_summand) + atoi(right_summand));
+			free(left_summand);
+			free(right_summand);
+
+		}
+
+		free(stack_read);
+	}
 
 	printf("Program successful, exitting");
 	return 0;
