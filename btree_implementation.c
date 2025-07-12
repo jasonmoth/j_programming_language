dd65767acb9e421e511abb8cb7405ea001f19099
Jason McAdam <jasonmcadam16@gmail.com>
2025-07-12 17:04:13 -0400

    Introduced Record struct

--- btree_implementation.c
+++ btree_implementation.c
@@ -3,26 +3,30 @@
 
 #define MAX_NUMBER_OF_KEYS 1
 
+typedef struct Record {
+	int key;
+	char* value;
+} Record;
+
 typedef struct Node {
 	struct Node* childNodes[MAX_NUMBER_OF_KEYS + 1];
-	int keys[MAX_NUMBER_OF_KEYS];
+	struct Record keys[MAX_NUMBER_OF_KEYS];
 } Node;
 
 typedef struct Btree {
 	Node* rootNodeAddress;
 } Btree;
 
-Btree* initialize_btree(int rootNodeKey) {
+Btree* initialize_btree() {
 
 	Node* n = malloc(sizeof(Node));
 	if (n == NULL) {
 		fprintf(stderr, "Error: Failed to malloc Node on btree initialization.\n");
 		exit(1);
 	}
-	n->keys[0] = rootNodeKey;
 
 	Btree* b = malloc(sizeof(Btree));
-	if (n == NULL) {
+	if (b == NULL) {
 		fprintf(stderr, "Error: Failed to malloc Btree on btree initialization.\n");
 		exit(1);
 	}
@@ -34,7 +38,8 @@ Btree* initialize_btree(int rootNodeKey) {
 
 int main() {
 
-	Btree* b = initialize_btree(5);
-	printf("b->rootNodeAddress = %p\n(b->rootNodeAddress)->keys[0] = %d\n", b->rootNodeAddress, (b->rootNodeAddress)->keys[0]);
+	Btree* b = initialize_btree();
+	printf("b->rootNodeAddress = %p\n", b->rootNodeAddress);
+	printf("(b->rootNodeAddress)->keys[0] = %d\n", (b->rootNodeAddress)->keys[0]);
 	return 0;
 }
